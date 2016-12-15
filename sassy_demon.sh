03f3d0fe643f5d09e91d6e17d79087cd554d3041
ghostanza <friedcaligari@gmail.com>
2016-12-15 10:41:47 -0500

    added flags and documentation

--- sassy_demon.sh
+++ sassy_demon.sh
@@ -23,29 +23,78 @@
 
 has_sass=$(which sass);
 has_inotifywait=$(which inotifywait);
+nosubs=false;
+watch_dir='';
+output_dir='';
+SCRIPT=$(basename ${BASH_SOURCE});
+RED='\033[0;31m';
+GREEN='\033[0;32m';
+CYAN='\033[0;36m';
+NC='\033[0m';
+
+function HELP {
+  echo -e "${CYAN}************ HELP DOCUMENTATION FOR SASSY_DEMON.SH ************${NC}\n";
+  echo -e "Dependencies:\t${SCRIPT} requires ${CYAN}inotify-tools${NC} and ${CYAN}sass${NC} to be installed\n"
+  echo -e "Basic Usage:\t${GREEN}./${SCRIPT} -w scss/${NC}\nThis will set ${SCRIPT} to watch the ${CYAN}scss/${NC} directory for changes.\n";
+  echo -e "Using Output Directory:\t${GREEN}./${SCRIPT} -w scss/ -o css/${NC}\nThis will set ${SCRIPT}to watch the ${CYAN}scss/${NC} directory for changes\nand output the compiled CSS file to the ${CYAN}css/${NC} directory.\n";
+  echo -e "${CYAN}\n********* FLAGS **********${NC}\n";
+  echo -e "${RED}(REQUIRED)${NC} [ ${GREEN}-w <path>${NC} ] is used to set the directory you want to watch. \nEx: ${GREEN}./${SCRIPT} -w scss/${NC} will set ${SCRIPT} to watch the ${CYAN}scss/${NC}\n";
+  echo -e "[ ${GREEN}-o <path>${NC} ] is used to set the directory you want to output to. \nEx: ${GREEN}./${SCRIPT} -w scss/ -o css/${NC} will set ${SCRIPT} to watch the ${CYAN}scss/${NC} but will output the CSS file to ${CYAN}css/${NC}\n${RED}NOTE:${NC} if no output directory is declared, ${SCRIPT} will output the CSS file to the same directory you are watching.\n";
+  echo -e "[ ${GREEN}-n${NC} ] is the ${CYAN}'no subdirectories'${NC} flag.\nThe program runs recursively, so without this flag, it will maintain the subdirectory path\nEx: If you're watching ${CYAN}scss/${NC} and your ouput directory is ${CYAN}css/${NC};\nif the file changed is ${CYAN}scss/test/files/testfile.scss${NC} then the CSS file will go to ${CYAN}css/test/files/testfile.css${NC}\nand will create the subdirectories if they do not already exist\nWith the ${GREEN}-n${NC} flag, the file would go to ${CYAN}/css/testfile.css${NC}\n";
+   echo -e "[ ${GREEN}-h${NC} ] will bring up this help documentation.\n";
+}
+
+while getopts :shw:o: FLAG; do
+  case $FLAG in
+    s)
+      nosubs=true
+      ;;
+    w)
+      watch_dir=$OPTARG
+      ;;
+    o)
+      output_dir=$OPTARG
+      ;;
+    h)
+      HELP
+      exit
+      ;;
+    \?)
+    echo -e "${RED}Invalid command line argument.${NC}\n"
+      HELP
+      exit
+      ;;
+  esac
+done
 
 if [ -z $has_inotifywait ] && [ -z $has_sass ]
 then
-    echo -e "********************\nERROR: YOU DO NOT HAVE CERTAIN DEPENDENCIES INSTALLED: \n- inotify-tools (https://github.com/rvoicilas/inotify-tools/wiki)\n - Sass (http://sass-lang.com/install)\n";
+    echo -e "${RED}********************\nERROR: YOU DO NOT HAVE CERTAIN DEPENDENCIES INSTALLED:${NC} \n- inotify-tools (https://github.com/rvoicilas/inotify-tools/wiki)\n - Sass (http://sass-lang.com/install)\n";
     exit;
 elif [ -z $has_inotifywait ]
 then
-    echo -e "********************\nERROR: YOU DO NOT HAVE CERTAIN DEPENDENCIES INSTALLED: \n- inotify-tools (https://github.com/rvoicilas/inotify-tools/wiki)\n";
+    echo -e "${RED}********************\nERROR: YOU DO NOT HAVE CERTAIN DEPENDENCIES INSTALLED:${NC} \n- inotify-tools (https://github.com/rvoicilas/inotify-tools/wiki)\n";
     exit;
 elif [ -z $has_sass ]
 then
-    echo -e "********************\nERROR: YOU DO NOT HAVE CERTAIN DEPENDENCIES INSTALLED: \n- Sass (http://sass-lang.com/install)\n";
+    echo -e "${RED}********************\nERROR: YOU DO NOT HAVE CERTAIN DEPENDENCIES INSTALLED:${NC} \n- Sass (http://sass-lang.com/install)\n";
     exit;
 fi
 
-if [ $# -eq 0 ]
+if [ -z $watch_dir ]
 then
-  echo "\nNo arguments specified. You must at least specify a directory to watch\n";
+  echo -e "\n${RED}No directories specified. You must at least specify a directory to watch${NC}\nUse ${GREEN}-w <path>${NC}\n";
   exit;
 else
-  watch_dir=$1;
-  output_dir=${2:-$watch_dir};
-  echo -e "--------------------------\nWATCHING DIRECTORY: ${watch_dir}\nWILL SEND OUTPUT TO: ${output_dir}\n--------------------------";
+  if [ -z $output_dir ]
+  then
+     output_dir=${watch_dir};
+  fi
+  echo -e "--------------------------\nWATCHING DIRECTORY: ${GREEN}${watch_dir}${NC}\nWILL SEND OUTPUT TO: ${GREEN}${output_dir}${NC}\n";
+  if [ $nosubs = true ]
+  then
+  echo -e "'NO SUBDIRECTORIES' flag being used\n";
+  fi
   inotifywait -mr -e modify $watch_dir |
     while read path action file; do
       if [[ ${file: -4} != ".swp" ]]
@@ -59,11 +108,14 @@ else
             elif [ "$output_dir" == '.' ]
             then
                 output_path=${path#$watch_dir};
+            elif [ $nosubs = true ]
+            then
+                output_path=${output_dir};
             else
                 output_path=${output_dir}${path#$watch_dir};
             fi
             mkdir -p $output_path;
-            echo -e "********\nCOMPILING: ${path}${filename}.scss \nOUTPUT: ${output_path}${filename}.css\n";
+            echo -e "********\nCOMPILING: ${GREEN}${path}${filename}.scss${NC} \nOUTPUT: ${GREEN}${output_path}${filename}.css${NC}\n";
             sass --sourcemap=none --cache=false ${path}${filename}.scss ${output_path}${filename}.css;
             #sass ${path}${filename}.scss ${output_path}${filename}.css;
             echo -e "done.\n**********";
